cf48e8554fdc4d6906725f4ca98912baf83f17df
Thijs van der Woude <thijsvanderwoude99@gmail.com>
2021-05-30 03:15:26 +0200

    Convert indentation to tabs

--- src/uname/uname.c
+++ src/uname/uname.c
@@ -22,51 +22,51 @@
 #define OPTIONS "amnrsvh"
 
 int main(int argc, char **argv) {
-    struct utsname buffer;
-    if (uname(&buffer) != 0) {
-        fprintf(stderr, "%s: error: %s\n", PN, strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+	struct utsname buffer;
+	if (uname(&buffer) != 0) {
+		fprintf(stderr, "%s: error: %s\n", PN, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc == 1) {
-        argc = 2;
-        argv[1] = "-s";
-    }
+	if (argc == 1) {
+		argc = 2;
+		argv[1] = "-s";
+	}
 
-    int opt;
-    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
-        switch (opt) {
-            case 'h':
-                fprintf(stdout, HELP);
-                fprintf(stdout, LICENSE);
+	int opt;
+	while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+		switch (opt) {
+			case 'h':
+				fprintf(stdout, HELP);
+				fprintf(stdout, LICENSE);
 				exit(EXIT_SUCCESS);
-            case 'a':
-                printf(
-                    "%s %s %s %s %s\n",
-                    buffer.sysname,
-                    buffer.nodename,
-                    buffer.release,
-                    buffer.version,
-                    buffer.machine
-                );
-                exit(EXIT_SUCCESS);
-            case 'm':
-                printf("%s ", buffer.machine);
-                break;
-            case 'n':
-                printf("%s ", buffer.nodename);
-                break;
-            case 'r':
-                printf("%s ", buffer.release);
-                break;
-            case 's':
-                printf("%s ", buffer.sysname);
-                break;
-            case 'v':
-                printf("%s ", buffer.version);
-                break;
-        }
-    }
-    printf("\n");
-    exit(EXIT_SUCCESS);
+			case 'a':
+				printf(
+					"%s %s %s %s %s\n",
+					buffer.sysname,
+					buffer.nodename,
+					buffer.release,
+					buffer.version,
+					buffer.machine
+				);
+				exit(EXIT_SUCCESS);
+			case 'm':
+				printf("%s ", buffer.machine);
+				break;
+			case 'n':
+				printf("%s ", buffer.nodename);
+				break;
+			case 'r':
+				printf("%s ", buffer.release);
+				break;
+			case 's':
+				printf("%s ", buffer.sysname);
+				break;
+			case 'v':
+				printf("%s ", buffer.version);
+				break;
+		}
+	}
+	printf("\n");
+	exit(EXIT_SUCCESS);
 }
